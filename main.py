22bf0b1bc6a6493da76bdb832fa50108b692254e
Илья Никулин <ilyanik2007@gmail.com>
2023-10-31 21:51:49 -0600

    added main for translation to ukr

---
+++ main.py
@@ -0,0 +1,12 @@
+import s2t
+import ukr_tts as ukr
+import sys 
+
+#  sys.argv[1] - model name 
+#  sys.argv[2] - audio file
+
+#s2t.s2t(sys.argv[1], sys.argv[2])
+
+# using deepl API for translation
+
+#ukr.transcribe('cpu', sys.argv[2]+".txt") # without voice cloning 
